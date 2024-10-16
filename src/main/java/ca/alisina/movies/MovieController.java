32c9e697ebc34c628a32c2ce416befc4e0d38205
Ali Sina <ali.sina1447@gmail.com>
2024-10-16 16:39:19 -0400

    UPDATE - RESTful API movie controller

--- src/main/java/ca/alisina/movies/MovieController.java
+++ src/main/java/ca/alisina/movies/MovieController.java
@@ -3,19 +3,22 @@ package ca.alisina.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
     @Autowired
-    private MovieService movieService;
+    private MovieRepository repository;  // Inject MovieRepository
+
+    @Autowired
+    private MovieService service;        // Inject MovieService (if used for additional logic)
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getMovies() {
+        List<Movie> movies = repository.findAll();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 }
