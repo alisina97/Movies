748282ef65654f7b400fcbed05fe152bffc6ab06
Ali Sina <ali.sina1447@gmail.com>
2024-10-16 16:37:56 -0400

    FIX -added collection name to movie entity

--- src/main/java/ca/alisina/movies/Movie.java
+++ src/main/java/ca/alisina/movies/Movie.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document
+@Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
