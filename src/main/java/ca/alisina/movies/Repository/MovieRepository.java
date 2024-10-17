9ba1d582da0b2a3b7946d0457db79103304342a8
Ali Sina <ali.sina1447@gmail.com>
2024-10-16 21:44:48 -0400

    ADD - find movie by imdb Id

--- src/main/java/ca/alisina/movies/Repository/MovieRepository.java
+++ src/main/java/ca/alisina/movies/Repository/MovieRepository.java
@@ -1,11 +1,13 @@
 package ca.alisina.movies.Repository;
 
-import ca.alisina.movies.DomainObject.Movie;
+import ca.alisina.movies.Model.Movie;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-
+    public Optional<Movie> findByImdbId(String ImdbId);
 }
