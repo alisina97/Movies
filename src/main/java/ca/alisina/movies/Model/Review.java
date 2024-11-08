89d2e7d97ef932c1d5f6f1efab3c7753ef22204c
Ali Sina <ali.sina1447@gmail.com>
2024-11-08 15:07:32 -0500

    FIX: db connection

---
+++ src/main/java/ca/alisina/movies/Model/Review.java
@@ -0,0 +1,20 @@
+package ca.alisina.movies.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document(collation = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+    public Review( String body) {
+        body=body;
+    }
+}
